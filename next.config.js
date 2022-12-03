b9eb5f52d11f6512851b054087aa15e71bfdbd5d
mamutovdmytro <dmitriy.mamutov@gmail.com>
2022-12-03 22:50:10 +0200

    fix next config

--- next.config.js
+++ next.config.js
@@ -9,30 +9,30 @@ const nextConfig = {
     defaultLocale: "en", // Should be the same as in i18n file
     locales: ["en"], // Should be the same as in i18n file
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2|mp4)$/,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-              name: "[name].[ext]",
-              outputPath: "video"
-          }
-      },
-        'file-loader',
-        {
-          loader: 'image-webpack-loader',
-          options: {
-            enforce: 'pre',
-          },
-        },
-      ],
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2|mp4)$/,
+  //     use: [
+  //       {
+  //         loader: "file-loader",
+  //         options: {
+  //             name: "[name].[ext]",
+  //             outputPath: "video"
+  //         }
+  //     },
+  //       'file-loader',
+  //       {
+  //         loader: 'image-webpack-loader',
+  //         options: {
+  //           enforce: 'pre',
+  //         },
+  //       },
+  //     ],
 
-    });
+  //   });
 
-    return config;
-  },
+  //   return config;
+  // },
 }
 
 module.exports = withPlugins(
