48324b619dc7edaaf5f2916afd3f28d0a9016d8c
mamutovdmytro <dmitriy.mamutov@gmail.com>
2022-12-04 17:45:39 +0200

    fix load video

--- containers/Home/components/Questions/index.js
+++ containers/Home/components/Questions/index.js
@@ -1,19 +1,36 @@
+import { useState, useEffect, useCallback } from "react";
 import Accordion from "components/Accordion";
-import Video from "components/Video";
 import { QUESTIONS_VIDEO, QUESTIONS_LIST } from "config/index";
 import useTranslation from "next-translate/useTranslation";
 import cn from "classnames";
 import Title from "components/Title";
+import dynamic from "next/dynamic";
+const Video = dynamic(() => import("components/Video"), {
+  ssr: false,
+});
 
 import styles from "./styles.module.scss";
 
 const Questions = () => {
   const { t } = useTranslation("index");
 
+  const [showComponent, setShowComponent] = useState(false);
+
+  const toggleVisibility = useCallback(() => {
+    if (showComponent === false && window.pageYOffset > 1) {
+      setShowComponent(true);
+      window.removeEventListener("scroll", toggleVisibility);
+    }
+  }, [showComponent]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+  }, [toggleVisibility]);
+
   return (
     <section className={cn(styles["questions"], "questions")}>
       <div className={cn(styles["questions__wrapper"], "questions__wrapper")}>
-        <Video src={QUESTIONS_VIDEO} />
+        {showComponent && <Video src={QUESTIONS_VIDEO} />}
 
         <div className="container">
           <Title size="h3" level={3} color="white">
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,10 @@
 import { Head, Html, Main, NextScript } from "next/document";
-import {WEBSITE_LOGO} from 'config/common';
+import { WEBSITE_LOGO } from "config/common";
 const Document = () => {
   return (
-    <Html lang='ua'>
+    <Html lang="ua">
       <Head>
-        <link className='logo' rel="icon" href={WEBSITE_LOGO} />
+        <link className="logo" rel="icon" href={WEBSITE_LOGO} />
         <link
           rel="preload"
           href="/static/fonts/Hellix/Hellix-Bold.woff2"
@@ -12,9 +12,16 @@ const Document = () => {
           type="font/woff2"
           crossOrigin="anonymous"
         />
-     <link
+        <link
           rel="preload"
           href="/static/fonts/Hellix/Hellix-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/static/fonts/Hellix/Hellix-Medium.woff2"
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
